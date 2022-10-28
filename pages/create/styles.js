48744296883aeb2a7dc959e134e9b30dc729315f
nychavs <chavesnaiury@gmail.com>
2022-10-28 10:20:38 -0300

    create (crud) finished

--- pages/create/styles.js
+++ pages/create/styles.js
@@ -7,11 +7,60 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    container1:{
+        flex:0.2,
+        width:'100%',
+        alignItems:'center',
+    },
+    container2:{
+        flex:1.5,
+        padding:20,
+        width:'100%',
+        backgroundColor:'#ddd',
+        alignItems:'center',
+    },
     texto1: {
         fontSize: 50,
+        color:'#fff',
+    },
+    texto2:{
+        fontSize:30,
+    },
+    top:{
+        flexDirection:'column',
+    },
+    campo:{
+        paddingBottom:10,
+        paddingTop:10,
+    }, 
+    caixa1:{
+        borderColor:'#222',
+        borderWidth:1,
+        borderRadius:8,
+        color:'#555',
+        height:40,
+        width:'100%',
+        padding:10,
+    },
+    foto0:{
+        padding:10,
+        alignItems: 'center',
+    },
+    foto1:{
+        width:150,
+        height:150,
+        backgroundColor:'#834989',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:10,
+    },
+    entrada:{
+        paddingBottom:10,
+        paddingTop:2,
+        alignItems:'center',
     },
     botao: {
-        width: '60%',
+        width: '30%',
         height: 50,
         backgroundColor: '#555',
         justifyContent: 'center',
@@ -19,11 +68,17 @@ const styles = StyleSheet.create({
         borderRadius: 8,
     },
     btn: {
-        width: '100%',
-        height: 60,
+        width: 150,
+        height: 40,
         padding: 10,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor:'yellow',
+        borderRadius:10,
+    },
+    sucesso:{
+        fontSize:20,
+        color:'#0f0',
     },
     txtButton: {
         fontFamily: 'Verdana',
