b24933dce593568f7a91ad2a4977bfae6773c053
nychavs <chavesnaiury@gmail.com>
2022-11-16 08:25:22 -0300

    delete and update

--- pages/delete/index.js
+++ pages/delete/index.js
@@ -1,56 +1,76 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, TouchableOpacity, FlatList} from 'react-native';
-import {FontAwesome} from '@expo/vector-icons';
-import {db} from '../login/firebaseConfig';
-import {doc, getDocs, collection, deleteDoc} from 'firebase/firestore';
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 import styles from './styles'
+import { FontAwesome } from '@expo/vector-icons'
+import { db } from '../login/firebaseConfig'
+import { doc, deleteDoc, getDocs, collection } from 'firebase/firestore'
+import { async } from '@firebase/util'
 
-export default function Delete({navigation}){
+export default function Delete ({ navigation }) {
+  const [page, setPage] = useState()
 
-    const [page, setPage] = useState()
+  useEffect(async () => {
+    const querySnapShot = await getDocs(collection(db, 'alunos'))
+    const list = []
+    querySnapShot.forEach(doc => {
+      list.push({...doc.data(),id: doc.id})
+    })
+    setPage(list)
+  }, [])
 
-    useEffect(async () => {
-        const querySnapShot = await getDocs(collection(db, 'alunos'))
-        const list = []
-        querySnapShot.forEach(doc =>{
-            list.push({ ...doc.data(), id: doc.id})
-        })
-        setPage(list)
-    },[])
+  function deleteItem (id) {
+    deleteDoc(doc(db, 'alunos', id))
+  }
 
-    function deleteItem(id){
-        deleteDoc(doc(db, 'alunos', id))
-    }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto1}>Delete</Text>
 
-    return(
-        <View style={styles.container}>
-            <Text style={styles.texto1}>
-                Delete
-            </Text>
-            <FlatList
-                data={page}
-                renderItem = {({item})=>{
-                    return(
-                        <View style={styles.pageDelete}>
-                            <TouchableOpacity 
-                            style={styles.deleteItem}
-                            onPress={()=>{deleteItem(item.id)}}
-                            >
-                                <FontAwesome
-                                    name='trash'
-                                    size={25}
-                                    color='#ff0'
-                                />
-                            </TouchableOpacity>
-                            <Text style={styles.txtDelete}>
-                                {item.name}   
-                            </Text>
-                            <Text style={styles.txtDelete}>
-                                {item.email}   
-                            </Text>
-                        </View>    
-                    )}}
-            />
-        </View>
-    )
+      <FlatList
+        data={page}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.pageDelete}>
+              <TouchableOpacity
+                style={styles.deleteItem}
+                onPress={() => {
+                  deleteItem(item.id)
+                }}
+              >
+                <FontAwesome name='trash' size={25} color='#ff0' />
+              </TouchableOpacity>
+
+              <Text
+                style={styles.txtDelete}
+                onPress={() => {
+                  navigation.navigate('Update', {
+                    id: item.id,
+                    name: item.name,
+                    image: item.image,
+                    email: item.email
+                  })
+                }}
+              >
+                {item.name}
+              </Text>
+
+              <Text
+                style={styles.txtDelete}
+                onPress={() => {
+                  navigation.navigate('Update', {
+                    id: item.id,
+                    name: item.name,
+                    image: item.image,
+                    email: item.email
+                  })
+                }}
+              >
+                {item.email}
+              </Text>
+            </View>
+          )
+        }}
+      />
+    </View>
+  )
 }
--- pages/update/index.js
+++ pages/update/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles'
 
-export default function Update({navigation}) {
+export default function Update({route, navigation}) {
     return (
         <View style={styles.container}>
             <Text style={styles.texto1}>
                 Update
             </Text>
+            <View>
+                <text>{route.params.name}</text>
+                <text>{route.params.email}</text>
+            </View>
         </View>
     )
 }
