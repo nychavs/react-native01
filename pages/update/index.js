33bc90304d7f1c15d5b2c29fdabdc0ca31b994c4
nychavs <chavesnaiury@gmail.com>
2022-11-23 11:06:38 -0300

    initialize update

--- pages/update/index.js
+++ pages/update/index.js
@@ -1,19 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles'
 
-export default function Update({route, navigation}) {
-    const [valor, setValor] = useState()
-
+export default function Update({navigation}) {
     return (
         <View style={styles.container}>
             <Text style={styles.texto1}>
                 Update
             </Text>
-            <View>
-                <text>{route.params!=null?route.params.name:''}</text>
-                {/* <text>{route.params.name}</text>
-                <text>{route.params.email}</text> */}
+            <View style={styles.btn}>
+                <TouchableOpacity 
+                    title='Delete'
+                    onPress={()=> navigation.navigate('Delete')}
+                    style={styles.botao}
+                >
+                   <Text style={styles.txtButton}>Delete</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
